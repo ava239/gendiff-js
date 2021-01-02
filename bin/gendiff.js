2880f2d417b1e399069f66ed791a7730201a7e66
ava <ava239@yandex.ru>
2021-01-02 05:33:59 +0300

    Configure help

---
+++ bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+
+program
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.');
+
+program.parse(process.argv);
