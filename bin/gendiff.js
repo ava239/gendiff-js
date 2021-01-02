78fe5a6f736956f61bcf92469683f818b0343d1b
ava <ava239@yandex.ru>
2021-01-02 05:54:18 +0300

    Add args and options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,9 @@ const { program } = require('commander');
 
 program
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .arguments('<filePath1> <filePath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
